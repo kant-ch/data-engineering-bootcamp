a24d2daf8c8c373b3aed97bf7f8e065502ec6e22
kant charoensedtasin <110617065+kant-ch@users.noreply.github.com>
2023-04-22 10:20:44 +0000

    22 Apr 2023

--- 01-introduction-to-data-engineering/workshops/data-engineering-lifecycle/load.sh
+++ 01-introduction-to-data-engineering/workshops/data-engineering-lifecycle/load.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -XPOST -H "Content-type: application/json" -d @dogs.json "https://getpantry.cloud/apiv1/pantry/<YOUR-PANTRY-ID>/basket/randomDogs"
+curl -XPOST -H "Content-type: application/json" -d @dogs.json "https://getpantry.cloud/apiv1/pantry/4818e9c4-653a-4454-96e5-f83dc5af1e6c/basket/randomDogs"
